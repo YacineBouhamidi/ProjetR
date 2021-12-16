a1a152103e246b4fcf360cb8dd0654b24724d275
YacineBouhamidi <94039093+YacineBouhamidi@users.noreply.github.com>
2021-12-16 21:01:27 +0100

    Add files via upload

---
+++ Application_Shiny/app.R
@@ -0,0 +1,5 @@
+library(shiny)
+
+runGitHub(repo= "appshiny" , username="bastiengev", ref="main")
+
+#https://github.com/bastiengev/appshiny pour retrouver le code r en détail ainsi que le dossier data et l'image utilisé)
